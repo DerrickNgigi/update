70ddba47f96190eb77729fffb3116d71dcb0438b
DerrickNgigi <45203387+DerrickNgigi@users.noreply.github.com>
2025-11-22 13:41:10 +0300

    Update GLOBAL_VERSION to 1.0.2

--- device_configs/FQX_SM_10008_globals.py
+++ device_configs/FQX_SM_10008_globals.py
@@ -1,7 +1,7 @@
 from machine import UART
 
 # ============ DEVICE CONFIGURATION ============ #
-GLOBAL_VERSION = "1.0.1"
+GLOBAL_VERSION = "1.0.2"
 
 # ====== Configuration ======
 UPDATE_URL = "https://raw.githubusercontent.com/DerrickNgigi/update/main"
