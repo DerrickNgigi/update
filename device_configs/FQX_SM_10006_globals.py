158fd32630b0eea8777f883bba205096388eb653
DerrickNgigi <ngigid0@gmail.com>
2026-01-24 08:07:57 +0300

    update sys

--- device_configs/FQX_SM_10006_globals.py
+++ device_configs/FQX_SM_10006_globals.py
@@ -1,7 +1,7 @@
 from machine import UART
 
 # ============ DEVICE CONFIGURATION ============ #
-GLOBAL_VERSION = "1.0.3"
+GLOBAL_VERSION = "1.0.7"
 
 # ====== Configuration ======
 UPDATE_URL = "https://raw.githubusercontent.com/DerrickNgigi/update/main"
